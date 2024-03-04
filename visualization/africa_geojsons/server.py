2706ca48defcfa6c2c304c0712aa52d7ae0dae6e
eaccooper5 <145416232+eaccooper5@users.noreply.github.com>
2024-03-04 16:48:34 -0500

    addition of final ETL and HTML to general folder and geojson data to visualization folder

---
+++ visualization/africa_geojsons/server.py
@@ -0,0 +1,14 @@
+# server.py
+import http.server
+import socketserver
+
+PORT = 8000
+
+class CORSRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        super().end_headers()
+
+with socketserver.TCPServer(("", PORT), CORSRequestHandler) as httpd:
+    print("Serving at port", PORT)
+    httpd.serve_forever()
